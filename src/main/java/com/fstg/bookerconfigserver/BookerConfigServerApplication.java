48d1606be68f06524ed58f9795d53985bb2067af
softcodeur <belkoweb9718gmail.com>
2021-12-06 13:33:44 +0100

    added eureka config

--- src/main/java/com/fstg/bookerconfigserver/BookerConfigServerApplication.java
+++ src/main/java/com/fstg/bookerconfigserver/BookerConfigServerApplication.java
@@ -3,7 +3,9 @@ package com.fstg.bookerconfigserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class BookerConfigServerApplication {
 
